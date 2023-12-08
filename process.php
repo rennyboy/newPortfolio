3c7830a0f2758527f7132cef6c3f63d5bab8359e
rennyboy <rennyboyjr@gmail.com>
2023-12-08 23:11:56 +0800

    update the email functionalities

--- process.php
+++ process.php
@@ -2,9 +2,9 @@
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
 	$name = $_POST["name"];
 	$email = $_POST["email"];
+	$mobile = $_POST["mobile"];
+	$subjectTitle = $_POST["subjectTitle"];
 	$message = $_POST["message"];
-    $mobile = $_POST["mobile"];
-    $subjectTitle = $_POST["subject"];
 	
 	// Validate email address
 	if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
@@ -16,18 +16,17 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 	$form_data = serialize(array(
 		"name" => $name,
 		"email" => $email,
-        "subject" => $subjectTitle,
-		"message" => $message,
-        "mobile" => $mobile,
-        
+		"mobile" => $mobile,
+		"subjectTitle" => $subjectTitle,
+		"message" => $message
 	));
 	
 	// Store serialized form data in a file
 	file_put_contents("form_data.txt", $form_data);
 	
-	$to = "rennyboyjr@gmail.com";
+	$to = "youremail@example.com";
 	$subject = "New Contact Form Submission";
-	$body = "Name: $name\nEmail: $email\nSubject: $subjectTitle\nMessage: $message\nMobile: $mobile";
+	$body = "Name: $name\nEmail: $email\nMobile Phone: $mobile\nSubject: $subjectTitle\nMessage: $message";
 	$headers = "From: $email\n";
 	$headers .= "Reply-To: $email\n";
 	mail($to, $subject, $body, $headers);
